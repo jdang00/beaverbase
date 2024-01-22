cc59f10bd218a8e1c0ca5f3a0e9e788ed24f1ec7
Justin Dang <jdang00@g.uafs.edu>
2024-01-22 01:50:26 -0600

    font changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import { type AppType } from "next/app";
 
+import { Noto_Sans } from 'next/font/google'
+
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+const noto = Noto_Sans({ subsets: ['latin'] })
+
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={noto.className}>
+    <Component {...pageProps} />
+    </main>
+
+  )
+    
 };
 
 export default api.withTRPC(MyApp);
